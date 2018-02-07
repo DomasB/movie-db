91dd1f9084662e8ab5ac024d316646818fdd0b01
Domas <domas.briedis@gmail.com>
2018-02-08 01:47:51 +0200

    Fixed bugs with zero values in data

--- src/App.vue
+++ src/App.vue
@@ -66,45 +66,54 @@ export default {
         })
       }
       var genres = [];
-      var genreObject = {};
+      var genreArray = [];
       this.movies.map(movie => {
         movie.Genre.split(', ').map(genre => {
           if (!genres.includes(genre)) genres.push(genre);
         })
       })
       var mappedData = this.movies.map(movie => {
+        var genreObject = {}
         var imdbRating = parseFloat(movie.imdbRating);
+        imdbRating = isNaN(imdbRating) ? 0 : imdbRating;
         var imdbVotes = parseInt(movie.imdbVotes.replace(",",""));
+        imdbVotes = isNaN(imdbVotes) ? 0 : imdbVotes;
+        var Metascore = isNaN(movie.Metascore) ? 0 : movie.Metascore;
+        var Year = isNaN(movie.Year) ? 0 : movie.Year;
         var Runtime = parseInt(movie.Runtime.replace(" mins",""));
+        var Runtime = isNaN(Runtime) ? 0 : Runtime;
         var BoxOffice = (!movie.BoxOffice || movie.BoxOffice == "N/A") ? 0 : parseInt(movie.BoxOffice.replace(/\$|,/ig, ""));
-
         var movieGenres = movie.Genre.split(', ');
         genres.map(genre => {
           genreObject[genre] = movieGenres.includes(genre) ? 1 : 0;
         })
-         return {imdbRating, imdbVotes, Runtime, BoxOffice, Year: movie.Year, Metascore: movie.Metascore, myScore: movie.myScore};
+        genreArray.push(genreObject);
+        return {Runtime, BoxOffice, imdbRating, imdbVotes, Year, Metascore, myScore: movie.myScore};
+
       })
+
       normalize('imdbRating', mappedData);
       normalize('imdbVotes', mappedData);
       normalize('Year', mappedData);
       normalize('Runtime', mappedData);
       normalize('Metascore', mappedData);
       normalize('BoxOffice', mappedData);
-      this.data = mappedData.map(o =>{
-        return {input: Object.assign(genreObject, { imdb: o.imdbRating, year: o.Year, votes: o.imdbVotes, time: o.Runtime, box: o.BoxOffice, mscore: o.Metascore}), output: { score: o.myScore/10 }};
+
+      this.data = mappedData.map((o,i) => {
+        return {input: Object.assign(genreArray[i], { imdb: o.imdbRating, year: o.Year, votes: o.imdbVotes, time: o.Runtime, box: o.BoxOffice, mscore: o.Metascore}), output: { score: o.myScore/10 }};
       });
-      console.log(this.data)
+console.log(this.data)
     },
     train() {
       this.transformData()
       net = new brain.NeuralNetwork({activation: this.activation});
-      net.train(this.data, { iterations: 40000, log: true, logPeriod: 10000 });
+      net.train(this.data, { iterations: 20000, log: true, logPeriod: 9999,  errorThresh: 0.002 });
       this.trained = true;
     },
     run(index) {
       this.transformData()
       var input = this.data[index].input
-      this.movies[index].myScore = Math.round(net.run(input).score*10);
+      this.movies[index].myScore = net.run(input).score*10;
     },
     save() {
       window.localStorage.setItem('movies', JSON.stringify(this.movies));
